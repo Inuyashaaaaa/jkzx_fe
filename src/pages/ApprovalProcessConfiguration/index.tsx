9f9d867d615cb49afe41a1d5f4f59d3485601e97
yanglu <yanglu@tongyu.tech>
2019-04-11 11:30:03 +0800

    更新任务与审批组关联

--- src/pages/ApprovalProcessConfiguration/index.tsx
+++ src/pages/ApprovalProcessConfiguration/index.tsx
@@ -1,20 +1,12 @@
 import PageHeaderWrapper from '@/lib/components/PageHeaderWrapper';
 import {
-  Button,
-  Checkbox,
-  Drawer,
-  notification,
-  Popconfirm,
-  Row,
-  Switch,
-  Tabs,
-  List,
-  Icon,
-  Select,
-} from 'antd';
-import React, { PureComponent } from 'react';
+  wkTaskApproveGroupList,
+  wkProcessStatusModify,
+  wkTaskApproveGroupCreateBatch,
+} from '@/services/approvalProcessConfiguration';
 import { wkApproveGroupList } from '@/services/auditing';
-import { wkTaskApproveGroupList } from '@/services/approvalProcessConfiguration';
+import { Button, Checkbox, Icon, List, notification, Select, Switch, Tabs, Modal } from 'antd';
+import React, { PureComponent } from 'react';
 import styles from './ApprovalProcessConfiguration.less';
 
 const TabPane = Tabs.TabPane;
@@ -22,8 +14,21 @@ const Option = Select.Option;
 
 class ApprovalProcessConfiguration extends PureComponent {
   public state = {
+    tabsData: [
+      {
+        tabName: '交易录入审批',
+        processName: '交易录入经办复合流程',
+      },
+      {
+        tabName: '出入金审批',
+        processName: '出入金经办复合流程',
+      },
+    ],
     approveGroupList: [],
     taskApproveGroupList: [],
+    loading: false,
+    currentProcessName: '交易录入经办复合流程',
+    visible: false,
   };
 
   constructor(props) {
@@ -35,22 +40,106 @@ class ApprovalProcessConfiguration extends PureComponent {
   };
 
   public fetchData = async () => {
+    this.setState({
+      loading: true,
+    });
     const requests = () =>
       Promise.all([
-        wkTaskApproveGroupList({ processName: '交易录入经办复合流程' }),
+        wkTaskApproveGroupList({ processName: this.state.currentProcessName }),
         wkApproveGroupList(),
       ]);
     const res = await requests();
     const [taskApproveGroupList, approveGroupList] = res;
-    console.log(approveGroupList.data, taskApproveGroupList.data);
     const error = res.some(item => {
       return item.error;
     });
 
-    if (error) return;
+    if (error) {
+      return this.setState({
+        loading: false,
+        taskApproveGroupList: [],
+      });
+    }
     this.setState({
+      loading: false,
       approveGroupList: approveGroupList.data || [],
       taskApproveGroupList: taskApproveGroupList.data || [],
+    });
+  };
+
+  public handleStatus = async e => {
+    // 更新流程状态
+    const { currentProcessName } = this.state;
+    const { data, error } = await wkProcessStatusModify({
+      processName: currentProcessName,
+      status: e,
+    });
+    if (error) return;
+    const { message } = error;
+    notification.success({
+      message: `流程${e ? '启用' : '停用'}成功`,
+      description: message,
+    });
+  };
+
+  public tabsChange = e => {
+    this.setState(
+      {
+        currentProcessName: e,
+      },
+      () => {
+        this.fetchData();
+      }
+    );
+  };
+
+  public handleApproveGroup = (e, taskId) => {
+    const { taskApproveGroupList } = this.state;
+    taskApproveGroupList.forEach(item => {
+      if (item.taskId === taskId) {
+        item.approveGroupList = e;
+      }
+    });
+    this.setState({
+      taskApproveGroupList,
+    });
+  };
+
+  public onSave = async () => {
+    // 判断所有节点均已分配了审批组，并弹窗确认
+    const { taskApproveGroupList } = this.state;
+    const isEvery = taskApproveGroupList.every(item => {
+      return item.approveGroupList && item.approveGroupList.length > 0;
+    });
+    if (!isEvery) {
+      return notification.error({
+        message: '确认所有审批节点都已选中审批组',
+      });
+    }
+    this.setState({
+      visible: true,
+    });
+  };
+
+  public handleOk = async () => {
+    this.setState({
+      visible: false,
+    });
+    const { currentProcessName, taskApproveGroupList } = this.state;
+    const { data, error } = await wkTaskApproveGroupCreateBatch({
+      processName: currentProcessName,
+      taskList: taskApproveGroupList,
+    });
+    if (error) return;
+    const { message } = error;
+    notification.success({
+      message: `保存成功`,
+    });
+  };
+
+  public handleCancel = e => {
+    this.setState({
+      visible: false,
     });
   };
 
@@ -67,7 +156,12 @@ class ApprovalProcessConfiguration extends PureComponent {
         }}
       >
         <div style={{ marginBottom: '20px' }}>
-          <Switch checkedChildren="开" unCheckedChildren="关" defaultChecked={true} />
+          <Switch
+            checkedChildren="开"
+            unCheckedChildren="关"
+            defaultChecked={true}
+            onClick={this.handleStatus}
+          />
           <span style={{ marginLeft: '6px' }}>启用流程</span>
         </div>
         <div style={{ marginTop: '60px' }}>
@@ -75,18 +169,16 @@ class ApprovalProcessConfiguration extends PureComponent {
           <p>
             <Checkbox>不允许审批自己发起的审批单</Checkbox>
           </p>
-          {/* <p><Checkbox>使用最末一个身披着的数据权限（不勾选则使用发起者的数据权限）</Checkbox></p> */}
         </div>
         <div className={styles.configButtonBox}>
           <p>
-            <Button type="primary">保存</Button>
+            <Button type="primary" onClick={this.onSave}>
+              保存
+            </Button>
           </p>
           <p>
             <Button>重置</Button>
           </p>
-          {/* <p>
-            <Button>预览流程图</Button>
-          </p> */}
           <p className={styles.message}>离开页面前，请确认所做的修改已经保存。</p>
         </div>
       </div>
@@ -97,88 +189,80 @@ class ApprovalProcessConfiguration extends PureComponent {
     return (
       <div className={styles.approvalProcessConfiguration}>
         <PageHeaderWrapper>
-          <Tabs defaultActiveKey="1">
-            <TabPane tab="交易录入审批" key="1">
-              {this.renderTabs()}
-              <div
-                style={{
-                  marginRight: '320px',
-                  background: '#FFF',
-                  padding: '30px',
-                  position: 'relative',
-                  height: '100%',
-                }}
-              >
-                <p className={styles.bolder}>审批流程</p>
-                <List bordered={false}>
-                  <List.Item>
-                    <div className={styles.approvalNode} style={{ background: '#e8e5e5' }}>
-                      <Icon type="more" style={{ width: '4px', color: '#999' }} />
-                      <Icon type="more" style={{ width: '4px', color: '#999' }} />
-                      <span style={{ marginLeft: '20px' }}>发起审批</span>
-                    </div>
-                    <span className={styles.approvalIcon}>{/* <Icon type="plus-circle" /> */}</span>
-                  </List.Item>
-                  {this.state.taskApproveGroupList.map(group => {
-                    return (
-                      <List.Item key={group.taskId}>
+          <Tabs defaultActiveKey="交易录入经办复合流程" onChange={this.tabsChange}>
+            {this.state.tabsData.map(tab => {
+              return (
+                <TabPane tab={tab.tabName} key={tab.processName}>
+                  {this.renderTabs()}
+                  <div
+                    style={{
+                      marginRight: '320px',
+                      background: '#FFF',
+                      padding: '30px',
+                      position: 'relative',
+                      height: '100%',
+                    }}
+                  >
+                    <p className={styles.bolder}>审批流程</p>
+                    <List bordered={false} loading={this.state.loading}>
+                      <List.Item>
                         <div className={styles.approvalNode} style={{ background: '#e8e5e5' }}>
                           <Icon type="more" style={{ width: '4px', color: '#999' }} />
                           <Icon type="more" style={{ width: '4px', color: '#999' }} />
-                          <span style={{ marginLeft: '20px' }}>{group.taskName}</span>
-                          <span className={styles.selectTile}>选择审批组</span>
-                          <Select
-                            defaultValue={(group.approveGroupDTO || []).map(
-                              dto => dto.approveGroupName
-                            )}
-                            style={{ width: 500 }}
-                            mode="multiple"
-                          >
-                            {this.state.approveGroupList.map(item => {
-                              return (
-                                <Option key={item.approveGroupId}>{item.approveGroupName}</Option>
-                              );
-                            })}
-                          </Select>
+                          <span style={{ marginLeft: '20px' }}>发起审批</span>
                         </div>
                         <span className={styles.approvalIcon}>
-                          {/* <Icon type="minus-circle" />  
-                            <Icon type="plus-circle" /> */}
+                          {/* <Icon type="plus-circle" /> */}
                         </span>
                       </List.Item>
-                    );
-                  })}
-                  {/* <List.Item>
-                    <div className={styles.approvalNode}  style={{ background: '#cce7f2' }}>
-                      <Icon type="more" style={{ width: '4px', color: '#999' }}/>
-                      <Icon type="more" style={{ width: '4px', color: '#999' }}/>
-                      <span style={{ marginLeft: '20px' }}>审批节点</span>
-                      <span className={styles.selectTile}>选择审批组</span>
-                      <Select
-                        defaultValue="a1"
-                        style={{ width: 500 }}
-                        mode="multiple"
-                      >
-                        <Option key='1'>1</Option>
-                        <Option key='2'>2</Option>
-                        <Option key='3'>3</Option>
-                      </Select>
-                    </div>
-                    <span className={styles.approvalIcon}>
-                      <Icon type="minus-circle" />  
-                      <Icon type="plus-circle" />
-                    </span>
-                  </List.Item>
-                  <List.Item>
-                    <div className={styles.approvalNode}  style={{ border: '1px dashed #e8e5e5' }}>
-                      <Button><Icon type="plus" style={{fontSize: '12px'}} />增加节点</Button>
-                    </div>
-                    <span className={styles.approvalIcon}></span>
-                  </List.Item> */}
-                </List>
-              </div>
-            </TabPane>
+                      {this.state.taskApproveGroupList.map(group => {
+                        return (
+                          <List.Item key={group.taskId}>
+                            <div className={styles.approvalNode} style={{ background: '#e8e5e5' }}>
+                              <Icon type="more" style={{ width: '4px', color: '#999' }} />
+                              <Icon type="more" style={{ width: '4px', color: '#999' }} />
+                              <span style={{ marginLeft: '20px' }}>{group.taskName}</span>
+                              <span className={styles.selectTile}>选择审批组</span>
+                              <Select
+                                defaultValue={(group.approveGroupDTO || []).map(
+                                  dto => dto.approveGroupName
+                                )}
+                                style={{ width: 500 }}
+                                mode="multiple"
+                                onChange={e => this.handleApproveGroup(e, group.taskId)}
+                              >
+                                {this.state.approveGroupList.map(item => {
+                                  return (
+                                    <Option key={item.approveGroupId}>
+                                      {item.approveGroupName}
+                                    </Option>
+                                  );
+                                })}
+                              </Select>
+                            </div>
+                            <span className={styles.approvalIcon}>
+                              {/* <Icon type="minus-circle" />  
+                                  <Icon type="plus-circle" /> */}
+                            </span>
+                          </List.Item>
+                        );
+                      })}
+                    </List>
+                  </div>
+                </TabPane>
+              );
+            })}
           </Tabs>
+          <Modal
+            title="消息提示"
+            visible={this.state.visible}
+            onOk={this.handleOk}
+            onCancel={this.handleCancel}
+          >
+            <p>
+              流程被修改后，所有进行中的审批单均会被重置，从第一个节点开始重新进行审批。是否确定保存修改？
+            </p>
+          </Modal>
         </PageHeaderWrapper>
       </div>
     );
