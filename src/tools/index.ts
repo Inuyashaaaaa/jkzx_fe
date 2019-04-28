b96e7c56b4fc74ca4519a2cb9feb78fd4d970878
lidan <lidan197@163.com>
2019-04-28 17:56:54 +0800

    鹰式重构

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -17,6 +17,7 @@ import { DigitalLegEuropean } from '@/domains/legs/DigitalLegEuropean';
 import { VerticalSpread } from '@/domains/legs/VerticalSpread';
 import { BarrierLeg } from '@/domains/legs/Barrier';
 import { DoubleSharkFin } from '@/domains/legs/DoubleSharkFin';
+import { Eagle } from '@/domains/legs/Eagle';
 
 export const isModelXY = data => {
   return (
@@ -182,6 +183,9 @@ export const getLegByProductType = (productType, exerciseType) => {
   }
   if (productType === PRODUCT_TYPE_MAP.DOUBLE_SHARK_FIN) {
     return DoubleSharkFin;
+  }
+  if (productType === PRODUCT_TYPE_MAP.EAGLE) {
+    return Eagle;
   }
   throw new Error('not match productType!');
 };
