e427aaf98120acae5da9dace781b9fc7385e8e0b
binyang <13956233265@163.com>
2019-05-12 11:34:55 +0800

    update ci

--- scripts/node-env.js
+++ scripts/node-env.js
@@ -22,7 +22,7 @@ process.exec('node -v', (nerror, nodeV) => {
     const result = nodeV === hope.node && yarnV === hope.yarn;
 
     console.log(`
-${!result ? '环境错误，请升级对应模块' : '环境匹配，可以正常启动'}
+${!result ? '环境错误，请升级对应模块' : '环境匹配，允许正常启动'}
 
 node: ${nodeV}${`${nodeV !== hope.node ? ` -> ${hope.node}` : ''}`}
 yarn: ${yarnV}${`${yarnV !== hope.yarn ? ` -> ${hope.yarn}` : ''}`}
