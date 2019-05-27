a5d0b692f61d93813aac90d396a69e306bf9c985
binyang <13956233265@163.com>
2019-05-27 17:54:10 +0800

    fix: 登录-验证码：点击验证码输入框，会挡住验证码图片

---
+++ src/layouts/UserLayout.tsx
@@ -0,0 +1,64 @@
+import GlobalFooter from '@/containers/GlobalFooter';
+import SelectLang from '@/containers/SelectLang';
+import { Icon } from 'antd';
+import React, { Fragment } from 'react';
+import DocumentTitle from 'react-document-title';
+import Link from 'umi/link';
+import { formatMessage } from 'umi/locale';
+import styles from './UserLayout.less';
+
+const logoPath = '/logo.svg';
+
+const links = [
+  {
+    key: 'help',
+    title: formatMessage({ id: 'layout.user.link.help' }),
+    href: '',
+  },
+  {
+    key: 'privacy',
+    title: formatMessage({ id: 'layout.user.link.privacy' }),
+    href: '',
+  },
+  {
+    key: 'terms',
+    title: formatMessage({ id: 'layout.user.link.terms' }),
+    href: '',
+  },
+];
+
+const copyright = (
+  <Fragment>
+    Copyright <Icon type="copyright" /> 同余科技
+  </Fragment>
+);
+
+class UserLayout extends React.PureComponent {
+  public render() {
+    const { children } = this.props;
+    return (
+      <DocumentTitle title={'欢迎登录 - 同余场外衍生品交易系统'}>
+        <div className={styles.container}>
+          <div className={styles.lang}>
+            <SelectLang />
+          </div>
+          <div className={styles.content}>
+            <div className={styles.top}>
+              <div className={styles.header}>
+                <Link to="/">
+                  <img alt="logo" className={styles.logo} src={logoPath} />
+                  <span className={styles.title}>场外衍生品交易管理系统</span>
+                </Link>
+              </div>
+              <div className={styles.desc} />
+            </div>
+            {children}
+          </div>
+          <GlobalFooter links={links} copyright={copyright} />
+        </div>
+      </DocumentTitle>
+    );
+  }
+}
+
+export default UserLayout;
