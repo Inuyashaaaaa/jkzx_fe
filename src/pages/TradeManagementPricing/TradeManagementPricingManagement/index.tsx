ff0ddfd609633ebc8108102deab109eb18c4cbc4
bzone <yarnbCoder@gmail.com>
2019-06-11 20:05:09 +0800

    fix: 修改不必要的三目

--- src/pages/TradeManagementPricing/TradeManagementPricingManagement/index.tsx
+++ src/pages/TradeManagementPricing/TradeManagementPricingManagement/index.tsx
@@ -599,7 +599,7 @@ const TradeManagementPricingManagement = props => {
                 onShowSizeChange: handleShowSizeChange,
                 showQuickJumper: true,
                 current: pagination.current,
-                pageSize: pagination.pageSize ? pagination.pageSize : PAGE_SIZE,
+                pageSize: pagination.pageSize,
                 pageSizeOptions: PAGE_SIZE_OPTIONS,
                 onChange: handlePaninationChange,
                 total: pagination.total,
