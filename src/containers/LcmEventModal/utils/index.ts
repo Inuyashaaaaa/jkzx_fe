94329a70015e02121a0d4c6b0fa1f07f596cc507
bzone <yarnbCoder@gmail.com>
2019-06-12 17:57:23 +0800

    fix: 交易管理-合约管理-亚式：观察页的【平均价】数据错误

--- src/containers/LcmEventModal/utils/index.ts
+++ src/containers/LcmEventModal/utils/index.ts
@@ -1,9 +1,18 @@
-import { OB_DAY_FIELD } from '@/constants/common';
-import { getMoment } from '@/tools';
+import { OB_DAY_FIELD, LEG_TYPE_MAP, BIG_NUMBER_CONFIG } from '@/constants/common';
+import { getMoment, getLegByRecord } from '@/tools';
 import BigNumber from 'bignumber.js';
 import moment from 'moment';
 
-export const countAvg = tableData => {
+export const countAvg = (tableData, data) => {
+  const leg = getLegByRecord(data);
+  if (leg.type === LEG_TYPE_MAP.ASIAN) {
+    return tableData
+      .reduce((sum, next) => {
+        return sum.plus(new BigNumber(next.weight || 0).multipliedBy(next.price || 0));
+      }, new BigNumber(0))
+      .decimalPlaces(BIG_NUMBER_CONFIG.DECIMAL_PLACES)
+      .toNumber();
+  }
   return tableData
     .reduce((sum, next) => {
       return sum.plus(next.price || 0);
