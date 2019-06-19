07a67aa8519d03f79521cf552cfa389879cc6820
yanglu <yanglu@tongyu.tech>
2019-06-19 19:01:51 +0800

    修改下拉框

--- src/pages/SystemSettingsUsers/ButtonSelect.tsx
+++ src/pages/SystemSettingsUsers/ButtonSelect.tsx
@@ -1,57 +1,39 @@
 import _ from 'lodash';
 import React, { memo, useEffect, useState } from 'react';
-import { Divider, Button, notification } from 'antd';
+import { Divider, Button, notification, Row } from 'antd';
 import { Form2, Select, Table2 } from '@/containers';
 
 const GroupSelcet = memo<any>(props => {
-  const [open, setOpen] = useState(true);
-  const [value, setValue] = useState(props.value);
-  const {
-    handleValueChanged,
-    defaultOpen,
-    autoSelect,
-    options,
-    mode,
-    record,
-    index,
-    form,
-    editing,
-  } = props;
+  const { cellApi, value: _value } = props;
+  const [value, setValue] = useState(_value);
+  const { options, mode, editing, onChange } = props;
 
   const handleOk = () => {
-    handleValueChanged(record, value);
-    setOpen(false);
-  };
-
-  const handleCancle = () => {
-    setOpen(false);
+    cellApi.saveCell();
   };
 
   return (
     <Select
-      open={open}
+      open={editing}
       editing={editing}
       autoSelect={true}
       options={options}
       mode={mode}
       value={value}
       onChange={val => {
+        onChange(val);
         setValue(val);
-      }}
-      onFocus={() => {
-        setOpen(true);
       }}
       dropdownRender={menu => {
         return (
           <>
             {menu}
-            <Divider style={{ margin: '4px 0' }} />
-            <div style={{ padding: '8px', cursor: 'pointer' }}>
-              <Button type="primary" style={{ marginRight: '15px' }} onClick={handleOk}>
+            <Divider style={{ margin: '6px 0' }} />
+            <Row type="flex" justify="end" style={{ padding: '8px' }}>
+              <Button size="small" type="primary" onClick={handleOk}>
                 确认
               </Button>
-              <Button onClick={handleCancle}>取消</Button>
-            </div>
+            </Row>
           </>
         );
       }}
