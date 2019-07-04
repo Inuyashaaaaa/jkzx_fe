2e62532a102fdb886e9caab4cfc800056ef9afae
yanglu <yanglu@tongyu.tech>
2019-07-04 17:37:58 +0800

    fix(tarde): 双鲨高行权价一定要大于低行权价

--- src/containers/legFields/LowBarrier.tsx
+++ src/containers/legFields/LowBarrier.tsx
@@ -1,10 +1,17 @@
-import { LEG_FIELD, RULES_REQUIRED, STRIKE_TYPES_MAP, UNIT_ENUM_MAP } from '@/constants/common';
-import { UnitInputNumber } from '@/containers/UnitInputNumber';
-import { legEnvIsBooking, legEnvIsPricing, getLegEnvs, getRequiredRule } from '@/tools';
-import { ILegColDef } from '@/types/leg';
 import FormItem from 'antd/lib/form/FormItem';
 import _ from 'lodash';
 import React from 'react';
+import { ValidationRule } from 'antd/lib/form';
+import {
+  LEG_FIELD,
+  RULES_REQUIRED,
+  STRIKE_TYPES_MAP,
+  UNIT_ENUM_MAP,
+  LEG_TYPE_MAP,
+} from '@/constants/common';
+import { UnitInputNumber } from '@/containers/UnitInputNumber';
+import { legEnvIsBooking, legEnvIsPricing, getLegEnvs, getRequiredRule } from '@/tools';
+import { ILegColDef } from '@/types/leg';
 import { Form2 } from '@/containers';
 
 export const LowBarrier: ILegColDef = {
@@ -17,10 +24,8 @@ export const LowBarrier: ILegColDef = {
     }
     return true;
   },
-  defaultEditing: record => {
-    return false;
-  },
-  render: (val, record, index, { form, editing, colDef }) => {
+  defaultEditing: record => false,
+  render: (valData, record, index, { form, editing, colDef }) => {
     // const { isBooking, isPricing, isEditing } = getLegEnvs(record);
 
     const getUnit = () => {
@@ -37,11 +42,35 @@ export const LowBarrier: ILegColDef = {
       return '%';
     };
 
+    const getRules = () => {
+      if (LEG_TYPE_MAP.DOUBLE_SHARK_FIN === record.$legType) {
+        return ([
+          {
+            message: '必须满足条件(低障碍价 < 低行权价 <= 高行权价)',
+            validator(rule, value, callback) {
+              if (
+                !(
+                  value < Form2.getFieldValue(record[LEG_FIELD.LOW_STRIKE]) &&
+                  Form2.getFieldValue(record[LEG_FIELD.LOW_STRIKE]) <=
+                    Form2.getFieldValue(record[LEG_FIELD.HIGH_STRIKE])
+                )
+              ) {
+                return callback(true);
+              }
+              return callback();
+            },
+          },
+        ] as ValidationRule[]).concat(getRequiredRule());
+      }
+      return [getRequiredRule()];
+    };
+
     return (
       <FormItem>
         {form.getFieldDecorator({
-          rules: [getRequiredRule()],
-        })(<UnitInputNumber autoSelect={true} editing={editing} unit={getUnit()} />)}
+          // rules: [getRequiredRule()],
+          rules: getRules(),
+        })(<UnitInputNumber autoSelect editing={editing} unit={getUnit()} />)}
       </FormItem>
     );
   },
