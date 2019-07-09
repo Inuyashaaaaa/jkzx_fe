d6c8a440d3fe29bac16b1b32a5789def88b8e247
yanglu <yanglu@tongyu.tech>
2019-07-09 15:29:15 +0800

    fix(trade): 交易管理-合约管理：一笔交易可以重复关联同一投资组合

--- src/pages/TradeManagementContractManage/PortfolioModalTable.tsx
+++ src/pages/TradeManagementContractManage/PortfolioModalTable.tsx
@@ -1,13 +1,13 @@
-import {
-  trdPortfolioListBySimilarPortfolioName,
-  trdTradePortfolioCreateBatch,
-} from '@/services/trade-service';
 import { Divider, message, Modal } from 'antd';
-import SmartTable from '@/containers/SmartTable';
 import FormItem from 'antd/lib/form/FormItem';
 import _ from 'lodash';
 import React, { PureComponent } from 'react';
 import uuidv4 from 'uuid/v4';
+import SmartTable from '@/containers/SmartTable';
+import {
+  trdPortfolioListBySimilarPortfolioName,
+  trdTradePortfolioCreateBatch,
+} from '@/services/trade-service';
 import ActionCol from './ActionCol';
 import { Form2, Select } from '@/containers';
 
@@ -16,11 +16,10 @@ class PortfolioModalTable extends PureComponent<
   any
 > {
   public state = {
-    modalVisible: false,
-    value: [],
     dataSource: [],
     options: [],
     portfolioNames: [],
+    formData: {},
   };
 
   private form: Form2 = null;
@@ -29,21 +28,13 @@ class PortfolioModalTable extends PureComponent<
     if (!this.props.rowData.portfolioNames) {
       return;
     }
-    const portfolioNames = this.props.rowData.portfolioNames;
+    const { portfolioNames } = this.props.rowData;
     this.setOptions();
     this.setState({
-      dataSource: portfolioNames.map(item => {
-        return {
-          uuid: uuidv4(),
-          portfolio: item,
-        };
-      }),
-    });
-  };
-
-  public showModal = () => {
-    this.setState({
-      modalVisible: true,
+      dataSource: portfolioNames.map(item => ({
+        uuid: uuidv4(),
+        portfolio: item,
+      })),
     });
   };
 
@@ -52,8 +43,9 @@ class PortfolioModalTable extends PureComponent<
   };
 
   public onRemove = params => {
-    const clone = [...this.state.dataSource];
-    const index = this.state.dataSource.findIndex(item => item.portfolio === params.portfolio);
+    const { dataSource } = this.state;
+    const clone = [...dataSource];
+    const index = dataSource.findIndex(item => item.portfolio === params.portfolio);
     clone.splice(index, 1);
     this.setState({
       dataSource: clone,
@@ -65,27 +57,40 @@ class PortfolioModalTable extends PureComponent<
     if (rsp.error) return;
     const { error, data } = await trdTradePortfolioCreateBatch({
       tradeId: this.props.rowData.tradeId,
-      portfolioNames: this.state.portfolioNames,
+      // portfolioNames: this.state.portfolioNames,
+      portfolioNames: Form2.getFieldsValue(this.state.formData).portfolioNames,
     });
     if (error) return;
-    const datas = this.state.portfolioNames.map(item => {
-      return {
-        uuid: uuidv4(),
-        portfolio: item,
-      };
-    });
+    const datas = this.state.portfolioNames.map(item => ({
+      uuid: uuidv4(),
+      portfolio: item,
+    }));
     message.success('添加成功');
-    this.setState({
-      dataSource: [...this.state.dataSource, ...datas],
-    });
+    const { dataSource } = this.state;
+    this.setState(
+      {
+        dataSource: [...dataSource, ...datas],
+        formData: {},
+      },
+      () => {
+        this.setOptions();
+      },
+    );
   };
 
   public onSearchFormChange = (props, fields, allFields) => {
-    this.setState({ portfolioNames: allFields.portfolioNames.value });
+    const { formData } = this.state;
+    this.setState({
+      portfolioNames: allFields.portfolioNames.value,
+      formData: { ...formData, ...fields },
+    });
   };
 
-  public setOptions = async (value?) => {
-    const options = await this.getOptions(value);
+  public setOptions = async value => {
+    let options = await this.getOptions(value);
+    options = options.filter(
+      item => !_.includes(this.state.dataSource.map(i => i.portfolio), item.value),
+    );
     this.setState({ options });
   };
 
@@ -109,46 +114,47 @@ class PortfolioModalTable extends PureComponent<
           width={800}
           title="关联投资组合"
           footer={false}
-          closable={true}
+          closable
           onCancel={this.handleCancel}
           visible={this.props.portfolioModalVisible}
         >
           <>
             <Form2
-              ref={node => (this.form = node)}
+              ref={node => {
+                this.form = node;
+              }}
               layout="inline"
               resetable={false}
               submitText="加入"
               onSubmit={this.handleCreate}
+              dataSource={this.state.formData}
               onFieldsChange={this.onSearchFormChange}
               columns={[
                 {
                   title: '请选择投资组合',
                   dataIndex: 'portfolioNames',
-                  render: (value, record, index, { form, editing }) => {
-                    return (
-                      <FormItem>
-                        {form.getFieldDecorator({
-                          rules: [
-                            {
-                              required: true,
-                              message: '投资组合为必填项',
-                            },
-                          ],
-                        })(
-                          <Select
-                            style={{ minWidth: 180 }}
-                            mode="multiple"
-                            placeholder="请输入内容搜索"
-                            showSearch={true}
-                            allowClear={true}
-                            onSearch={this.setOptions}
-                            options={this.state.options}
-                          />
-                        )}
-                      </FormItem>
-                    );
-                  },
+                  render: (value, record, index, { form, editing }) => (
+                    <FormItem>
+                      {form.getFieldDecorator({
+                        rules: [
+                          {
+                            required: true,
+                            message: '投资组合为必填项',
+                          },
+                        ],
+                      })(
+                        <Select
+                          style={{ minWidth: 180 }}
+                          mode="multiple"
+                          placeholder="请输入内容搜索"
+                          showSearch
+                          allowClear
+                          onSearch={this.setOptions}
+                          options={this.state.options}
+                        />,
+                      )}
+                    </FormItem>
+                  ),
                 },
               ]}
             />
@@ -156,7 +162,7 @@ class PortfolioModalTable extends PureComponent<
             <SmartTable
               title={() => '已加入的投资组合'}
               rowKey="uuid"
-              bordered={true}
+              bordered
               dataSource={this.state.dataSource}
               size="small"
               columns={[
@@ -166,15 +172,13 @@ class PortfolioModalTable extends PureComponent<
                 },
                 {
                   title: '操作',
-                  render: (value, record, index) => {
-                    return (
-                      <ActionCol
-                        params={record}
-                        rowData={this.props.rowData}
-                        onRemove={this.onRemove}
-                      />
-                    );
-                  },
+                  render: (value, record, index) => (
+                    <ActionCol
+                      params={record}
+                      rowData={this.props.rowData}
+                      onRemove={this.onRemove}
+                    />
+                  ),
                 },
               ]}
             />
