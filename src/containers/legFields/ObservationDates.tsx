7facb5dd5ae813971352ee29892c9aeeca61e77d
yanglu <yanglu@tongyu.tech>
2019-07-10 18:12:52 +0800

    fix(trade): 批量生成观察日时，观察日起始日不对

--- src/containers/legFields/ObservationDates.tsx
+++ src/containers/legFields/ObservationDates.tsx
@@ -149,7 +149,6 @@ class ObserveModalInput extends InputBase<
     // 亚式，区间累积,单鲨
     const start = getMoment(Form2.getFieldValue(record[LEG_FIELD.EFFECTIVE_DATE]))
       .clone()
-      .add(1, 'days')
       .format('YYYY-MM-DD');
     const end = getMoment(Form2.getFieldValue(record[LEG_FIELD.EXPIRATION_DATE])).format(
       'YYYY-MM-DD',
