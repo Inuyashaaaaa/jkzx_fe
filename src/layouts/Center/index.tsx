7012bba124da1e98e4b11268d9beb67b5c0a4069
yl <1334193743@qq.com>
2019-10-23 15:26:42 +0800

    fix: 标签页名称改为场外报告库估值与风险监测系统

--- src/layouts/Center/index.tsx
+++ src/layouts/Center/index.tsx
@@ -1,15 +1,18 @@
 import React, { memo } from 'react';
 import { BackTop } from 'antd';
+import DocumentTitle from 'react-document-title';
 import Content from './Content';
 import Layout from './Layout';
 import Sider from './Sider';
 
 const ChartTalk = memo(props => (
-  <Layout>
-    <Sider></Sider>
-    <Content>{props.children}</Content>
-    <BackTop style={{ right: '10px', bottom: '10px' }} />
-  </Layout>
+  <DocumentTitle title="场外报告库估值与风险监测系统">
+    <Layout>
+      <Sider></Sider>
+      <Content>{props.children}</Content>
+      <BackTop style={{ right: '10px', bottom: '10px' }} />
+    </Layout>
+  </DocumentTitle>
 ));
 
 export default ChartTalk;
