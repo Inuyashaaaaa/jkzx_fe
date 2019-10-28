b3fcb72629cfa296d52fa95ba4f034e511ab5c80
yl <1334193743@qq.com>
2019-10-28 16:29:33 +0800

    feat: 前端页面2小时无操作自动登出

--- src/layouts/Center/index.tsx
+++ src/layouts/Center/index.tsx
@@ -1,18 +1,56 @@
-import React, { memo } from 'react';
+import React, { memo, useEffect, useState } from 'react';
 import { BackTop } from 'antd';
+import { connect } from 'dva';
+import _ from 'lodash';
 import DocumentTitle from 'react-document-title';
 import Content from './Content';
 import Layout from './Layout';
 import Sider from './Sider';
 
-const ChartTalk = memo(props => (
-  <DocumentTitle title="场外报告库估值与风险监测系统">
-    <Layout>
-      <Sider></Sider>
-      <Content>{props.children}</Content>
-      <BackTop style={{ right: '10px', bottom: '10px' }} />
-    </Layout>
-  </DocumentTitle>
-));
+const ChartTalk = props => {
+  sessionStorage.setItem('lastTime', new Date().getTime());
+  let currentTime = new Date().getTime();
+  const timeOut = 2 * 60 * 60 * 1000;
+  const { user } = props;
+  let timer;
 
-export default ChartTalk;
+  const operateTimeOut = () => {
+    currentTime = new Date().getTime();
+    const lastTime = sessionStorage.getItem('lastTime');
+    if (currentTime - lastTime > timeOut) {
+      window.clearInterval(timer);
+      props.dispatch({
+        type: 'login/logout',
+        payload: {
+          loginUrl: '/center/login',
+          userId: _.get(user, 'currentUser.username'),
+        },
+      });
+    }
+  };
+
+  const onClick = () => {
+    sessionStorage.setItem('lastTime', new Date().getTime());
+  };
+
+  useEffect(() => {
+    timer = window.setInterval(operateTimeOut, 1000);
+  }, []);
+
+  return (
+    <DocumentTitle title="场外报告库估值与风险监测系统">
+      <Layout onClick={onClick}>
+        <Sider></Sider>
+        <Content>{props.children}</Content>
+        <BackTop style={{ right: '10px', bottom: '10px' }} />
+      </Layout>
+    </DocumentTitle>
+  );
+};
+
+export default memo(
+  connect(({ login, user }) => ({
+    user,
+    login,
+  }))(ChartTalk),
+);
