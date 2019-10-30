91a4a3e67408a63845ff0937464ff12ef3e43793
yl <1334193743@qq.com>
2019-10-30 09:32:50 +0800

    fix: 数据加载异常情况，点击详情到bct系统中无此类错误信息显示

--- src/tools/notificationDom.tsx
+++ src/tools/notificationDom.tsx
@@ -5,7 +5,7 @@ export const messageDom = (
   <div style={{ fontSize: '14px', color: '#fff' }}>
     数据加载出现一些异常
     <Divider type="vertical" style={{ background: '#00E8E8' }} />
-    <a style={{ color: '#00E8E8' }} href="/system-settings/operation-log">
+    <a style={{ color: '#00E8E8' }} href="/system-settings/operation-log?activeKey=error">
       查看详情
     </a>
   </div>
