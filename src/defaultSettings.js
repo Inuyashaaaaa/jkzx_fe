f85b60d96f5c9541829628f97e6264805cfcce46
yanglu <yanglu@tongyu.tech>
2020-06-08 16:09:11 +0800

    Update defaultSettings.js

--- src/defaultSettings.js
+++ src/defaultSettings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  version: 'BCT_JKZX_1.2.0',
+  version: 'BCT_JKZX_1.2.1',
   navTheme: 'dark', // theme for nav menu
   layout: 'sidemenu', // nav menu position: sidemenu or topmenu
   contentWidth: 'Fluid', // layout of content: Fluid or Fixed, only works when layout is topmenu
